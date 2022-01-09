bf91985ead1fedab7c99c597ed4d072b30aaa627
Rafael Dell'Aquila <rafael@dellaquila.dev>
2022-01-09 15:35:15 -0300

    :wrench: starter configs

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,8 +3,6 @@
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
@@ -12,16 +10,6 @@ export default defineComponent({
 
   components: {},
 
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
-  },
+  setup() {},
 });
 </script>
